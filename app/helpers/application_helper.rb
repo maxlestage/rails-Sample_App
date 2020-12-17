1fc93697fe715777ea19464a4d92688c3b9cf2be
Max <maxlestage@icloud.com>
2020-12-17 17:08:11 +0100

    first helper base_titre

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def titre
+  def title
     base_titre = "Simple App du Tutoriel Ruby on Rails"
-    if @titre.nil? # Si titre est vide ou non remplit
+    if @title.nil? # Si titre est vide ou non remplit
       base_titre # affiche base_titre uniquement
     else
-      "#{base_titre} | #{@titre}" # Affiche "base_titre |" +"@titre".  @titre que l'on récupère dans notre pages_controller.rb
+      "#{base_titre} | #{@title}" # Affiche "base_titre |" +"@titre".  @titre que l'on récupère dans notre pages_controller.rb
     end
   end
 end
