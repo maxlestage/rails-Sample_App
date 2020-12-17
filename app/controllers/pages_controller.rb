faa9ec8a4326343bf447d16c1ce676be240ae7d3
Max <maxlestage@icloud.com>
2020-12-17 16:21:06 +0100

    Fini avec les pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Accueil'
   end
 
   def contact
+    @title = 'Contactez nous!'
+  end
+
+  def about
+    @title = 'À propos'
   end
 end
